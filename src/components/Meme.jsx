fb93a8e1bff82b225066080aa59d2f86f55cdc6d
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-04-03 23:11:00 +0530

    feat: Meme component Added

---
+++ src/components/Meme.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Meme() {
+   return (
+      <main>
+         <form className="form">
+            <input type="text" placeholder="Shut up" className="form-input" />
+            <input
+               type="text"
+               placeholder="and take my money"
+               className="form-input"
+            />
+            <button className="form-button">Get a new meme image 🖼</button>
+         </form>
+      </main>
+   );
+}
+
+export default Meme;

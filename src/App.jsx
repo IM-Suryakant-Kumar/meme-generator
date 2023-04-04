38edb3ec509e2fbd3d661d92ea51788d24d68903
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-04-04 17:28:13 +0530

    refactor: Updated challenge 6 doing it again with useState()

--- src/App.jsx
+++ src/App.jsx
@@ -105,12 +105,16 @@ import Meme from "./components/Meme";
 // }
 // -------------------------Challenge 6------------------------
 export default function App() {
-   const isGoingOut = true;
+   const [isGoingOut, setIsGoingOut] = React.useState(true);
+
+   function changeMind() {
+      setIsGoingOut((prevState) => !prevState);
+   }
 
    return (
       <div className="state">
          <h1 className="state-title">Do I feel like going out tonight?</h1>
-         <div className="state-value">
+         <div className="state-value" onClick={changeMind}>
             <h1>{isGoingOut ? "Yes" : "No"}</h1>
          </div>
       </div>
